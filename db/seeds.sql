2b546b3b3448266107e70b107035b320a0bdc058
Faras Momin <famomin@utexas.edu>
2017-02-17 09:55:03 -0600

    added 4 bugers/sandwiches on burgers table

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+USE INSERT INTO burgers
+(burger_name)
+VALUES
+('Chik-fil-A Spicy Deluxe Chicken Sandwich'),
+('Whataburger Patty Melt'),
+('Hopdoddy El Diablo'),
+('The Buger Joint BBQ Double Patty Burger');
