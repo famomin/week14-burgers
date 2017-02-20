26d691d7c91ee7b80922ff625cd20e6e1aba2a9d
Faras Momin <famomin@utexas.edu>
2017-02-19 18:27:18 -0600

    added routes for get, post and put

---
+++ controllers/burger_controller.js
@@ -0,0 +1,44 @@
+//rependencies
+var express = require("express");
+
+var router = express.Router();
+
+// Import the model (cat.js) to use its database functions.
+var burger = require("../models/burger.js");
+
+//get route to show all the burgers on home page.
+router.get("/", function (req, res) {
+  burger.selectAll(function (results){
+    var burgerObject = {
+      burgers: results
+    };
+    console.log(burgerObject);
+    res.render("index", burgerObject);
+  });
+});
+
+//post route to allow user to add a new burger.
+router.post("/", function (req, res) {
+  burger.insertOne ([
+    "burger_name", "devoured"
+  ], [
+    req.body.burger_name, req.body.devoured
+  ], function () {
+      res.redirect("/");
+    });
+});
+
+//put route  for to update existing burgers
+router.put("/:id", function (req, res) {
+  var condition = "id = " + req.params.id;
+  console.log("condition", condition);
+
+  burger.updateOne({
+    devoured: req.body.devoured
+  }, condition, function() {
+    res.redirect("/");
+  });
+});
+
+// Export routes for server.js to use.
+module.exports = router;
