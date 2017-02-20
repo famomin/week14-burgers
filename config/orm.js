8e558f24da976f154bb20153f09eca8bb3857a71
Faras Momin <famomin@utexas.edu>
2017-02-19 18:25:04 -0600

    added orm object with queries for show all, add and update burgers

--- config/orm.js
+++ config/orm.js
@@ -1,9 +1,35 @@
 //requiring connection.js for orm.js
-var connection = require("./connection.js");
+var mysql = require("mysql");
 
-//orm object with
+//orm object with sql query
 var orm = {
+  selectAll: function(res) {
+    connection.query("SELECT * FROM burgers", function(err, results) {
+      if (err){
+        throw err;
+      }
+      res.render("index", { burgersAvail: results });
+    });
+  };
 
+  insertOne: function (res, newBurger) {
+    connection.query("INSERT INTO burgers (burger_name) VALUES (?)", [newBurger], function(err, results){
+      if (err) {
+        throw err;
+      }
+      res.redirect("/");
+    });
+  };
+
+  //updating burgers only if they are
+  updateOne = function (res, updateBurger) {
+    connection.query("UPDATE burgers SET devoured = ? WHERE id = ?", [false, updateBurger], function(err, result) {
+      if (err) {
+        throw err;
+      }
+      res.redirecct("/");
+    }
+  }
 };
 
 //exporting orm object
