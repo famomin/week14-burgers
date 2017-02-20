8f017d53b1c545238e261f7a929d798e10b27bbf
Faras Momin <famomin@utexas.edu>
2017-02-19 22:20:29 -0600

    changed orm with help of cats orm

--- config/orm.js
+++ config/orm.js
@@ -1,34 +1,83 @@
-//requiring connection.js for orm.js
-var mysql = require("mysql");
+//created using CATS apps from in calss activity
+
+//requiring connection.js for orm.js to get mysql connection
+var connection = require('../config/connection.js');
+
+//  Helper function for SQL syntax.
+function printQuestionMarks(num) {
+  var arr = [];
+
+  for(var i = 0; i < num; i++) {
+    arr.push("?");
+  }
+
+  return arr.toString();
+}
+
+// Helper function for SQL syntax.
+function objToSql(ob) {
+  var arr = [];
+
+  for(var key in ob) {
+    if (Object.hasOwnProperty.call(ob, key)) {
+      arr.push(key + "=" + ob[key]);
+    }
+  }
+
+  return arr.toString();
+}
 
 //orm object with sql query
 var orm = {
-  selectAll: function(res) {
-    connection.query("SELECT * FROM burgers", function(err, results) {
-      if (err){
-        throw err;
-      }
-      res.render("index", { burgersAvail: results });
-    });
-  };
-
-  insertOne: function (res, newBurger) {
-    connection.query("INSERT INTO burgers (burger_name) VALUES (?)", [newBurger], function(err, results){
+  //to show all the available
+  selectAll: function(tableInput, cb) {
+    var queryString = "SELECT * FROM " + tableInput + ";";
+    connection.query(queryString, function(err, result) {
       if (err) {
         throw err;
       }
-      res.redirect("/");
+      cb(result);
     });
-  };
+  },
+
+  //to insert new burger
+  insertOne: function(table, cols, vals, cb) {
+    var queryString = "INSERT INTO " + table;
+
+    queryString += " (";
+    queryString += cols.toString();
+    queryString += ") ";
+    queryString += "VALUES (";
+    queryString += printQuestionMarks(vals.length);
+    queryString += ") ";
+
+    console.log(queryString);
+
+    connection.query(queryString, vals, function(err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  },
 
   //updating burgers only if they are
-  updateOne = function (res, updateBurger) {
-    connection.query("UPDATE burgers SET devoured = ? WHERE id = ?", [false, updateBurger], function(err, result) {
+  update: function(table, objColVals, condition, cb) {
+    var queryString = "UPDATE " + table;
+
+    queryString += " SET ";
+    queryString += objToSql(objColVals);
+    queryString += " WHERE ";
+    queryString += condition;
+
+    console.log(queryString);
+
+    connection.query(queryString, function(err, result) {
       if (err) {
         throw err;
       }
-      res.redirecct("/");
-    }
+      cb(result);
+    });
   }
 };
 
